908eedfad4574754e2aa5dd64c348fde111efae1
韦安广 <2378038243@qq.com>
2025-04-08 10:17:42 +0800

    最后提交

--- db/index.js
+++ db/index.js
@@ -5,10 +5,9 @@ const mysql = require('mysql')
 // 创建与数据库的连接
 const db = mysql.createPool({
 	host:'localhost',
-	user:'back_system',
+	user:'back',
 	password:'123456',
-	database:'back_system'
+	database:'back'
 })
-
 // 对外暴露数据库
 module.exports = db
